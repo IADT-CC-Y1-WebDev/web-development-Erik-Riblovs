8d9c70b9b9126e893c7bb817b5b9200319e43261
Eriks Riblovs <N00254119@iadt.ie>
2026-01-19 12:52:21 +0000

    My first commit

--- src/exercises/01-php-introduction/01-variables.php
+++ src/exercises/01-php-introduction/01-variables.php
@@ -27,6 +27,12 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+        $firstName = "Eriks";
+        $lastName = "Riblovs";
+        $age = 18;
+        $city = "Wicklow";
+
+        echo "My name is $firstName $lastName, I am $age years old and I live in $city.";
         ?>
     </div>
 
