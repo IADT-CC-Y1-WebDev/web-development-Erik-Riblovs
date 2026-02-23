73ac7d954b97c3d5c99ee72034646dc085dc3fd5
Eriks Riblovs <N00254119@iadt.ie>
2026-02-23 13:02:16 +0000

    Started Project

---
+++ src/project/books/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Books Project</h1>
+        <ul>
+            <li><a href="book_list.php">Books</a></li>
+        </ul>
+    </div>
+</body>
+</html>
