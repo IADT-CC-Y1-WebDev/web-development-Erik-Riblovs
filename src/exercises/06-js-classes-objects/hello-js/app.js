8cc063e5c4941ae492be0d28825fa5c320872183
Eriks Riblovs <N00254119@iadt.ie>
2026-03-02 12:57:05 +0000

    started javascript

---
+++ src/exercises/06-js-classes-objects/hello-js/app.js
@@ -0,0 +1,13 @@
+let user = {
+    firstName: "Erik",
+    lastName: "Riblovs",
+    age: 19,
+    hobbies: ["gooning", "gooning contests with the boys", "sword figthing"],
+    friend: {
+        firstName: "Jeffery",
+        lastName: "Epstein",
+        age: "unc"
+    }
+};
+
+console.log(user.friend)
